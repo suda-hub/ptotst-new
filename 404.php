224c8f819f2cb5cec01622da2157a44cc4e3364a
suda <suda@starcraft-n.co.jp>
2023-06-23 13:28:21 +0900

    フォルダリセット

--- 404.php
+++
@@ -1,6 +0,0 @@
-<?php get_header();?>
-<main class="error">
-	<h2 class="error404__lead">404エラー</h2>
-	<p class="error404__txt">ページが存在しません</p>
-</main>
-<?php get_footer();?>
--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer role="contentinfo" class="l-footer">
-	
-</footer>
-
-<script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/init.min.js"></script>
-<?php wp_footer(); ?>
-</body>
-</html>
